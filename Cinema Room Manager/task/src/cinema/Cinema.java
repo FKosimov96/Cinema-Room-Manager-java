0f8ee215443aaa77ff37c2b0b9ae99786d789e49
FKosimov <fayzcorp96@gmail.com>
2023-03-27 00:12:07 +0300

    First Stage is completed

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -4,5 +4,22 @@ public class Cinema {
 
     public static void main(String[] args) {
         // Write your code here
+        System.out.println("Cinema:");
+        System.out.print("  ");
+        for(int t = 1; t <=8; t++){
+            System.out.print(t + " ");
+        }
+
+        for (int i = 1; i <8; i++) {
+            if(i == 1) System.out.println("");
+            System.out.print(i+ " ");
+            for (int j = 0; j < 8; j++){
+                System.out.print("S ");
+                if(j == 7){
+                    System.out.println("");
+                }
+            }
+        }
+        System.out.println("");
     }
 }
